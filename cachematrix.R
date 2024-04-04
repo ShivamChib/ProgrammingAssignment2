2576b8689cea74d537e17ee0ea7483104e1efa1b
ShivamChib <108234615+ShivamChib@users.noreply.github.com>
2024-04-04 11:01:41 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Function to calculate the mean of a numeric vector with caching
+cached_mean <- function(x) {
+  
+  # Check if cached result exists
+  if (!exists("cached_mean_result")) {
+    # If not, compute the mean and cache the result
+    cat("Computing mean...\n")
+    cached_mean_result <<- mean(x)
+  } else {
+    # If cached result exists, use it
+    cat("Using cached mean...\n")
+  }
+  
+  # Return the cached result
+  return(cached_mean_result)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Example usage
+x <- c(1, 2, 3, 4, 5)
+print(cached_mean(x))  # Computes and caches the mean
+print(cached_mean(x))  # Uses cached mean
